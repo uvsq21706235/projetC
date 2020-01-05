ecc792c904d838cba41d8a70d92d091b96c38651
Utilisateur <user@debian.arcahe.ovh>
2020-01-05 15:13:28 +0100

    ajout connect sur pompe p12, p13, p23

--- application.cpp
+++ application.cpp
@@ -5,6 +5,10 @@ Application::Application(QWidget*parent):S(new Simulateur), M(parent) {
 	connect(M->vt12, SIGNAL(clicked(bool)), this, SLOT(powerVanne(S.getVanneVT12())));
 	connect(M->vt23, SIGNAL(clicked(bool)), this, SLOT(powerVanne(S.getVanneVT23())));
 
+	connect(M->v12, SIGNAL(clicked(bool)), this, SLOT(powerVanne(S.getVanneV12())));
+	connect(M->v13, SIGNAL(clicked(bool)), this, SLOT(powerVanne(S.getVanneV13())));
+	connect(M->v23, SIGNAL(clicked(bool)), this, SLOT(powerVanne(S.getVanneV23())));
+
 	connect(M->p12, SIGNAL(clicked(bool)), this, SLOT(powerPompe(S.getPompe12())));
 	connect(M->p22, SIGNAL(clicked(bool)), this, SLOT(powerPompe(S.getPompe22())));
 	connect(M->p32, SIGNAL(clicked(bool)), this, SLOT(powerPompe(S.getPompe32())));
